7b67333b00da20d2237253212d95264e969207bc
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-01 16:19:29 +0600

    ph closure

---
+++ script.js
@@ -0,0 +1,20 @@
+//Closure
+
+// function increment() {
+//   let counter = 0;
+//   return function count() {
+//     return (counter += 1);
+//   };
+// }
+
+// const counterIncrement = increment();
+// console.log(counterIncrement());
+// console.log(counterIncrement());
+// console.log(counterIncrement());
+// console.log(counterIncrement());
+
+// let myCounter = 0;
+// console.log((myCounter += 1));
+// console.log(++myCounter);
+// //console.log(myCounter++);
+// console.log(myCounter);
